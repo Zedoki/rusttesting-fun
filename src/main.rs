d1496c283be2037e03c12b43595223c8e74d39f9
Zedoki <zedoki@gmail.com>
2019-12-28 22:37:06 +0100

    code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+    println!("Devine le nombre!");
+
+    println!("Entre un nombre à deviner");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    
 }
 
 
