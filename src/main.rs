316edb78b7a81b40cdfdc09398527e019bd88be6
Thierry Tran <ttran@aubay.com>
2020-05-02 22:25:31 +0200

    todo

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Hello, world!");
     println!("Devine le nombre!");
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("le nombre secret est: {}", secret_number);
     println!("Entre un nombre à deviner");
 
     let mut guess = String::new();
@@ -14,6 +18,12 @@ fn main() {
 
     println!("You guessed: {}", guess);
     
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("too small"),
+        Ordering::Greater => println!("too big"),
+        Ordering::Equal => println!("this is it!"),
+
+    }
     
 }
 
